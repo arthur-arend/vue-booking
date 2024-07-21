6a285e74b2d0f94c7f9fb96fe01e94aa028673d1
Arthur Arend <arthur.nox@gmail.com>
2024-07-21 03:42:41 -0300

    First commit

---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,14 @@
+// Styles
+import 'vuetify/styles'
+import '@mdi/font/css/materialdesignicons.css'
+import '@fortawesome/fontawesome-free/css/all.css'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives
+})
---
+++ src/views/AboutView.vue
@@ -0,0 +1,70 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <v-select label="Cidade" :items="cities"></v-select>
+        <v-btn color="primary">Pesquisar</v-btn>
+        <v-menu
+          v-model:menu="menu"
+          :close-on-content-click="false"
+          transition="scale-transition"
+          offset-y
+        >
+          <template #activator="{ props }">
+            <v-btn v-bind="props">Open Menu</v-btn>
+          </template>
+
+          <v-date-picker elevation="24"></v-date-picker>
+        </v-menu>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import { defineComponent, ref } from 'vue'
+import { VApp, VMain, VContainer, VBtn, VSelect, VMenu, VDatePicker } from 'vuetify/components'
+import axios from 'axios'
+
+export default defineComponent({
+  components: {
+    VApp,
+    VMain,
+    VContainer,
+    VBtn,
+    VSelect,
+    VMenu,
+    VDatePicker
+  },
+  setup() {
+    const cities = ref([])
+    const menu = ref(false)
+
+    const fetchCities = async () => {
+      try {
+        const response = await axios.get('http://localhost:3001/cities')
+        cities.value = response.data
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchCities()
+
+    return {
+      cities,
+      menu
+    }
+  }
+})
+</script>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
